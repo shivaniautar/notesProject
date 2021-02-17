cc1eb8363effb577ed7ab0fe29535081c1e7b431
Shivani Autar <shivaniautar18@gmail.com>
2021-02-16 21:12:38 -0800

    notes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./server/config/mongoose.config'); // This is new
+require('./server/config/mongoose.config'); // Making connection to MongoDB
+
+
+//Express is middleware
 app.use(cors());
 app.use(express.json()); // This is new
 app.use(express.urlencoded({ extended: true })); // This is new
